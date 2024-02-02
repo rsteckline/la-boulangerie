e85f404b0556b8fcaa53249e8be0a5e1af68014f
Bobby Steckline <rjsturing@gmail.com>
2024-02-01 19:58:51 -0500

    Refactor and update app structure and components

---
+++ src/components/BreadList/BreadList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './BreadList.css'; 
+
+const BreadList = () => {
+
+  return (
+    <div className="breadList">
+
+    </div>
+  );
+};
+
+export default BreadList;
