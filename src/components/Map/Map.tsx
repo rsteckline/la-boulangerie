274a2bd38162bb8d6d0be5b59acbe6575af6f085
Bobby Steckline <rjsturing@gmail.com>
2024-02-08 15:51:43 -0500

    Add detail page routing

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -5,13 +5,25 @@ import countriesGeoJson from '../../data/countries.json';
 import { useNavigate } from 'react-router-dom';
 import './Map.css';
 
+const maxBounds = L.latLngBounds(
+  L.latLng(-90, -180),
+  L.latLng(90, 180)
+);
+
 const MapComponent = () => {
   return (
-    <MapContainer center={[0, 0]} zoom={2} style={{ height: '80vh', width: '100%' }}>
+    <MapContainer 
+      center={[0, 0]} 
+      zoom={2} 
+      style={{height: '80vh', width: '100%'}}
+      maxBounds={[[90, -180], [-90, 180]]}
+      minZoom={2} 
+      maxZoom={8} 
+    >
       <TileLayer
         url="https://tile.thunderforest.com/mobile-atlas/{z}/{x}/{y}.png?apikey=a7eda3efcc6b40449d697372a8171c3b"
         attribution='&copy; <a href="http://thunderforest.com">Thunderforest</a> contributors'
-        noWrap={false}
+        noWrap={true}
       />
       <GeoJSONLayer />
     </MapContainer>
@@ -38,12 +50,11 @@ const GeoJSONLayer = () => {
         });
       },
     });
-      
 
     geoJsonLayer.addTo(map);
   }, [map, navigate]);
 
   return null;
-}
+};
 
 export default MapComponent;
