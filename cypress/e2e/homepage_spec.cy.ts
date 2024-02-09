61fbc19c0ff07adde6315dfa715fdfdc62b2bdfa
Bobby Steckline <rjsturing@gmail.com>
2024-02-09 12:03:18 -0500

    Add beginning tests

---
+++ cypress/e2e/homepage_spec.cy.ts
@@ -0,0 +1,29 @@
+describe("Boulangerie Main Page", () => {
+  beforeEach(() => {
+    cy.intercept(
+      'GET',
+      'https://rails-bread-face5cd9a02c.herokuapp.com/api/v1/countries/Brazil',
+      { 
+        statusCode: 200,
+        fixture: "/mock-country.json",
+      }
+    )
+    cy.visit('/');
+  });
+
+  it("should display the title of the application", () => {
+    cy.get('h1').contains("Belongea's Boulangerie");
+  });
+
+  it("should have a working home button that navigates to '/'", () => {
+    cy.get('.nav-links a').contains('Home').click();
+    cy.visit('/');
+    cy.url().should('eq', Cypress.config('baseUrl'));
+    cy.location("pathname").should("eq", "/");
+  });
+
+  it("should display a map container", () => {
+    cy.get('.map-container').should('be.visible');
+  });
+
+});
