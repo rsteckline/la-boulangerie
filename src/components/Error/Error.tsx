e36ed3f84b261075bbd3c46764294bd0a74f9a91
Deanna Stevens <deanna.sofias@gmail.com>
2024-02-09 17:13:16 -0500

    Add error component styling

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Error = () => {
   return (
     <div className="error-container">
-      <h2 className="error-message">This page doesn't exist.</h2>
+      <h2 className="error-message">Sorry, this page doesn't exist.</h2>
     </div>
   );
 }
