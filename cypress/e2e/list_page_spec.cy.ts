8ba314cb297ba23a079b2050122a8c127067b962
Bobby Steckline <rjsturing@gmail.com>
2024-02-09 12:46:06 -0500

    Add list page testing

--- cypress/e2e/list_page_spec.cy.ts
+++ cypress/e2e/list_page_spec.cy.ts
@@ -1,5 +1,31 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("BreadList Page", () => {
+  beforeEach(() => {
+    cy.intercept(
+      'GET',
+      'https://rails-bread-face5cd9a02c.herokuapp.com/api/v1/breads/Brazil',
+      {
+        statusCode: 200,
+        fixture: "/mock-country.json",
+      }
+    );
+    cy.visit('/breads/Brazil');
+  });
+
+  it("should display the country name and description", () => {
+    cy.get('h2').contains('Breads from Brazil');
+    cy.get('p').contains('Pão de queijo is a famous Brazilian cheese bread');
+  });
+
+  it("should list the breads for that country", () => {
+    const breads = ["Pão de Queijo", "Beiju", "Bolo de Milho", "Broa"];
+    breads.forEach(bread => {
+      cy.get('ul').contains(bread);
+    });
+  });
+
+  it("should navigate to the detail page of a bread when clicked", () => {
+    cy.get('a').contains('Pão de Queijo').click();
+    const expectedUrl = encodeURI('/breads/Brazil/Pão de Queijo');
+    cy.url().should('include', expectedUrl);
+  });
+});
