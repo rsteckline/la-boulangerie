6c28b19e9aec054292961a0a8ee4e183a08256f9
Deanna Stevens <deanna.sofias@gmail.com>
2024-02-27 21:46:24 -0500

    Adjust component with Tailwind breakpoints

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -5,9 +5,9 @@ import MapComponent from '../Map/Map';
 const Main = () => {
 
   return (
-    <div className="main">
-      <div className='map-wrapper'>
-        <h2>Select a country from our world bakery</h2>
+    <div className="main h-screen w-screen flex justify-center items-center">
+      <div className='map-wrapper flex flex-col h-[90%] w-[90%] items-center'>
+        <h2 className='text-xl md:text-2xl lg:text-3xl pt-4 md:pt-14 lg:pt-15'>Select a country from our world bakery</h2>
       <div className="map-background">
         <div className="map-container">
           <MapComponent />
