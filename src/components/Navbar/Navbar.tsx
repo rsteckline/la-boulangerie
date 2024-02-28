df63578afe233546e897ecdd0b691d70e8bc1b57
Deanna Stevens <deanna.sofias@gmail.com>
2024-02-27 19:34:56 -0500

    Adjust small breakpoint sizing

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
     location.pathname.split("/").length >= 3;
 
   return (
-    <div className="navbar bg-[#82aa9f] shadow-[0_-2px_4px_rgba(0,0,0,0.1)] fixed w-full h-12 z-[1000] flex justify-between items-center p-4 left-0 bottom-0">
+    <div className="navbar bg-[#82aa9f] shadow-[0_-2px_4px_rgba(0,0,0,0.1)] fixed w-full h-14 z-[1000] flex justify-between items-center p-4 left-0 bottom-0">
       <h1>
         <Link to="/" className="app-title">
           Belongea's Boulangerie
